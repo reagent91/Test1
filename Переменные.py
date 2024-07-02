7707d402934dd32696023cbfee73d1c2771cd4cf
reagent91 <familykuryanov@gmail.com>
2024-07-02 10:55:19 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,4 @@
+Homework_done=12 #Количество выполненных домашних заданий
+Hours_done=1.5 #Количество затраченных часов
+Name='Python'
+print(Name,',всего задач:',Homework_done,',затраченных часов:',Hours_done,'среднее время выполнения',(Hours_done/Homework_done))
