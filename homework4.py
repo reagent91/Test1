68113945cf7f7c00f1d53a50ba43b01d2fa88e22
reagent91 <familykuryanov@gmail.com>
2024-07-03 10:54:03 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string= input('Есть только ')
+print('У самурая нет цели, есть только',my_string)
+print(len('У самурая нет цели, есть только')+len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[-1])
