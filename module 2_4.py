76e420958b4ce993d00a99386e1bcad9d9e5de35
reagent91 <familykuryanov@gmail.com>
2024-08-12 02:59:45 +0300

    Add files via upload

---
+++ module 2_4.py
@@ -0,0 +1,24 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+is_prime = True
+primes = []
+not_primes = []
+
+for i in numbers:
+    if i == 1:
+        numbers.remove(1)
+        break
+for i in numbers:
+    for j in range(2, i):
+        if i ** 0.5 >= j and i % j == 0:
+            is_prime = False
+            break
+        else:
+            is_prime = True
+    if is_prime == True:
+        primes.append(i)
+    else:
+        not_primes.append(i)
+
+print('Простые числа', primes)
+print('Сложные числа', not_primes)
